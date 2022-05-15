ee93e2b823adf5f5e45259f8f248c9033a65d15b
Guillermoscript <arepayquezo@gmail.com>
2022-05-15 18:13:36 -0400

    All App Modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,11 @@ import { ClientModule } from './client/client.module';
 import { AgentsModule } from './agents/agents.module';
 import { AbilityModule } from './ability/ability.module';
 import { PersonModule } from './person/person.module';
+import { InsuranceCarrierModule } from './insurance-carrier/insurance-carrier.module';
+import { BranchTypesModule } from './branch-types/branch-types.module';
+import { SubBranchsModule } from './sub-branchs/sub-branchs.module';
+import { PoliciesModule } from './policies/policies.module';
+import { PolicyStatusModule } from './policy-status/policy-status.module';
 
 @Module({
   imports: [
@@ -20,6 +25,11 @@ import { PersonModule } from './person/person.module';
     AgentsModule,
     AbilityModule,
     PersonModule,
+    InsuranceCarrierModule,
+    BranchTypesModule,
+    SubBranchsModule,
+    PoliciesModule,
+    PolicyStatusModule,
   ],
   controllers: [],
   providers: [],
