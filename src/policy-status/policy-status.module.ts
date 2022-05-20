a0a67344af50398a2d5b71edfd4017ecdbca8cfa
Enyeltov <enyeltov.16@gmail.com>
2022-05-20 11:40:28 +0200

    Add AbilityFactory

--- src/policy-status/policy-status.module.ts
+++ src/policy-status/policy-status.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PolicyStatusService } from './policy-status.service';
 import { PolicyStatusController } from './policy-status.controller';
+import { AbilityFactory } from 'src/ability/ability.factory';
 
 @Module({
   controllers: [PolicyStatusController],
-  providers: [PolicyStatusService]
+  providers: [PolicyStatusService, AbilityFactory],
 })
 export class PolicyStatusModule {}
