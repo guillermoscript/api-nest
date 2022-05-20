4f32588c8023780bd2b4c884e2e65e6d943d6ef9
Enyeltov <enyeltov.16@gmail.com>
2022-05-20 11:40:07 +0200

    Change Dto birthdate to type string & add abilityFactory to modules

--- src/person/person.module.ts
+++ src/person/person.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { PersonController } from './person.controller';
+import { AbilityFactory } from 'src/ability/ability.factory';
 
 @Module({
   controllers: [PersonController],
-  providers: [PersonService],
+  providers: [PersonService, AbilityFactory],
 })
 export class PersonModule {}
