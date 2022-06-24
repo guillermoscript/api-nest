a89f7c156627219045b31eb600b47b3ef36a63a5
Enyeltov <enyeltov.16@gmail.com>
2022-06-24 09:48:53 +0200

    User controller change

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { AbilityFactory } from 'src/ability/ability.factory';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AbilityFactory],
 })
 export class UserModule {}
